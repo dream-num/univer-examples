2d29df8bd3b5c5a79d7f852b5f57a4a5e363f809
Dushusir <1414556676@qq.com>
2024-04-03 22:15:59 +0800

    feat: add conditional formatting

--- sheets/locales.ts
+++ sheets/locales.ts
@@ -1,8 +1,12 @@
 import { LocaleType } from '@univerjs/core'
 
 import { zhCN, enUS } from 'univer:locales'
-
+import { enUS as UniverSheetsUIEnUS } from '@univerjs/sheets-ui';
+import { enUS as UniverSheetsConditionalFormattingUIEnUS } from '@univerjs/sheets-conditional-formatting-ui';
 export const locales = {
     [LocaleType.ZH_CN]: zhCN,
-    [LocaleType.EN_US]: enUS
+    [LocaleType.EN_US]: {
+        ...enUS,
+        ...UniverSheetsConditionalFormattingUIEnUS
+    }
 };
